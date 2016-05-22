2420ef22d653b8e2b140dfe057db25627da11ef2
unknown <afdalwahyu@gmail.com>
2016-05-22 12:16:42 +0700

    add stuff for friend controller

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -6,20 +6,32 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\User
+
 class FriendController extends Controller
 {
     public function show($id)
     {
-
+      $user = App\User::where('id',$id)->firstOrFail();
+      return response()->json($user);
     }
 
     public function showlist($id)
     {
-
+      $list = App\Friend::where('user_id_action',$id)->orWhere('user_id_response',$id)->firstOrFail();
+      return response()->json($list);
     }
 
     public function addfriend(Requests $request, $id)
     {
+      $user = Auth::user();
+      $request = new App\Friend;
+      $request->user_id_action = $user->id;
+      $request->user_id_response = $id;
+      $request->description = $request->description;
+      $request->save();
+
+      return response()->json(['status' => 'success','message' => 'success add as friend']);
 
     }
 }
