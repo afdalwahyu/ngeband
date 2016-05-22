42233941d515544640a12e282ef5002ac174b51f
unknown <afdalwahyu@gmail.com>
2016-05-22 09:52:42 +0700

    add some routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,28 +27,42 @@ Route::group(['prefix' => 'api'], function () {
   //routes for authenticated user only
   Route::group(['middleware' => 'auth'], function () {
 
-    //show user profile
-    Route::get('user/profile', 'UserController@showProfile');
+    //group for user
+    Route::group(['prefix' => 'user'], function () {
+      //show user profile
+      Route::get('profile', 'UserController@showProfile');
+      //update user profile
+      Route::put('update', 'UserController@updateProfile');
+      //get list band created by user
+      Route::get('band','UserController@showband');
+      //accept or decline pending friend request
+      Route::put('friend/{id}','UserController@updatefriend');
+      //accept or decline pending band request
+      Route::put('band/{id}','UserController@updateband');
+    }
 
-    //update user profile
-    Route::post('user/update', 'UserController@updateProfile');
+    //group for band
+    Route::group(['prefix' => 'band'], function () {
+      //create
+      Route::post('store','BandController@store');
+      //show band member list
+      Route::get('{id}','BandController@show');
+      //join list
+      Route::post('join/{id}','BandController@join');
+      //show member band list
+      Route::get('list/{id}','BandController@list')
+    }
 
-    //-------band------//
-    //create band
-    //show band member list
-    //join band
-    //edit band member (only creator)
-    //accept/decline pending member
-    //-----------------//
 
-    //-------friend------//
-    //show friend
-    //add friend
-    //approve friend
-    //decline friend
-    //unfriend
-    //-------------------//
-
+    //group for friend
+    Route::group(['prefix' => 'friend'], function () {
+      //show profile based on id
+      Route::get('{id}','FriendController@show')
+      //show list friend based on id
+      Route::get('list/{id}','FriendController@showlist')
+      //add as friend
+      Route::post('add/{id}','FriendController@addfriend')
+    }
   });
 
 });
